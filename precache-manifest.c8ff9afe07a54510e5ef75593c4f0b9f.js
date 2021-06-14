f3f206a802a8da2b728fdfeb09f3405ff73ed57b
Mayra <mayra@iMac-de-Mayra.local>
2021-06-14 13:22:46 -0700

    Updates

---
+++ precache-manifest.c8ff9afe07a54510e5ef75593c4f0b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6efc8d59eec979160930",
+    "url": "/sbdtoken/static/css/main.da0ca175.chunk.css"
+  },
+  {
+    "revision": "6efc8d59eec979160930",
+    "url": "/sbdtoken/static/js/main.6efc8d59.chunk.js"
+  },
+  {
+    "revision": "6ac8da6402ad8ddabaad",
+    "url": "/sbdtoken/static/css/1.143f17d1.chunk.css"
+  },
+  {
+    "revision": "6ac8da6402ad8ddabaad",
+    "url": "/sbdtoken/static/js/1.6ac8da64.chunk.js"
+  },
+  {
+    "revision": "99d5a5f327a8e30ccd96",
+    "url": "/sbdtoken/static/js/runtime~main.99d5a5f3.js"
+  },
+  {
+    "revision": "8179dd91129252ae7b4e657ead2e39b2",
+    "url": "/sbdtoken/static/media/logo.8179dd91.png"
+  },
+  {
+    "revision": "2fb54d1482730f281e126e1c710448b4",
+    "url": "/sbdtoken/index.html"
+  }
+];
